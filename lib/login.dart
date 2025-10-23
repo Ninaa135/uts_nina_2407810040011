647e3a9399694b769d70b9371b908724a3c9ff40
Nninaa135 <nina.lp3i.krw24@gmail.com>
2025-10-23 20:23:52 +0700

    15 kelima

--- lib/login.dart
+++ lib/login.dart
@@ -8,39 +8,62 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  bool _showpassword = true;
   final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Image(
-                image: AssetImage("gambar/logo.png"),
-                width: 150,
-                height: 150,
-              ),
-              Text("LKS MART",
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage("gambar/logo.png"),
+              width: 150,
+              height: 150,
+            ),
+            Text(
+              "LKS MART",
               style: TextStyle(
-                fontSize: 50, fontWeight: FontWeight.bold, color: Colors.blueAccent
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
               ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 5,
+                children: [
+                  Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
+            ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 5,
-                  children: [
-                    Text("Sign In", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,),),
-                  Text("Enter your ID and password to sign in!", style: TextStyle(fontSize: 10, fontWeight: FontWeight.normal, color: Colors.grey),),
-                  ],
-                ),
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 5,
+                children: [
+                  Text(
+                    "Enter your ID and password to sign in!",
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.normal, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
 
-              Padding(padding: const EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -52,16 +75,37 @@ class _loginState extends State<login> {
                     decoration: InputDecoration(
                       hintText: "email",
                       border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  Text("Password*"),
+                  TextField(
+                    controller: _passwordcontroller,
+                    obscureText: _showpassword,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showpassword = !_showpassword;
+                          });
+                        },
+                        icon: Icon(
+                          _showpassword ? Icons.remove_red_eye : Icons.visibility_off,
+                        ),
+                      ),
+                      hintText: "Min. 8 characters",
+                      border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8)
                       )
                     ),
                   ),
-                  Text("Password*")
                 ],
-              ),)
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
